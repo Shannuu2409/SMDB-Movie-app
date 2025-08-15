b974bf6eee1158f71e9b9d8cb79c143a52e66d3e
Shannuu2409 <shannu2409@gmail.com>
2025-08-15 09:25:22 +0530

    Fix: Implement search functionality by passing query to Index component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import { Navbar } from "./components/Navbar";
 import Index from "./Index";
 import MoviesPage from "./pages/MoviesPage";
@@ -7,6 +7,15 @@ import AnimePage from "./pages/AnimePage";
 import { ProfilePage } from "./components/ProfilePage";
 import { AuthProvider } from "./contexts/AuthContext";
 
+// A new component to handle reading the URL query
+const IndexWithSearch = () => {
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const searchQuery = queryParams.get("search") || "";
+
+  return <Index searchQuery={searchQuery} />;
+};
+
 function App() {
   return (
     <AuthProvider>
@@ -14,7 +23,8 @@ function App() {
         <div className="App">
           <Navbar />
           <Routes>
-            <Route path="/" element={<Index />} />
+            {/* Use the new component for the root path */}
+            <Route path="/" element={<IndexWithSearch />} />
             <Route path="/movies" element={<MoviesPage />} />
             <Route path="/tv-series" element={<TVSeriesPage />} />
             <Route path="/anime" element={<AnimePage />} />
